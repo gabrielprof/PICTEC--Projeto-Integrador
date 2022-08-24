8fb8619b4d528d57d8cd3d5de37fccb36aa920ec
gabrielprof <gabrielfilosofiaprofessor@gmail.com>
2022-08-24 08:59:08 -0400

    Realiza ajustes gerais

--- scripts.js
+++ scripts.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
-  var ajustaLarguraMapa = function() {
-    var larguraTela = document.body.clientWidth;
+
+  var resize1000px = function() {
+    let larguraTela = document.body.clientWidth;
     if (larguraTela < 1000) {
+      $('#texto-capa').removeClass('col-5');
+      $('#carrossel').removeClass('col-7');
       $('#mapa').removeClass('col-7');
       $('#mapa').css("padding-top", 15);
     } else if (larguraTela >= 1001) {
+      $('#texto-capa').addClass('col-5');
+      $('#carrossel').addClass('col-7');
       $('#mapa').addClass('col-7');
     };
   };
+
+  var resize520px = function() {
+    let larguraResize520 = document.body.clientWidth;
+    if (larguraResize520 < 520) {
+      $('.icones-rodape').removeClass("col-6");
+      $('.rodape-texto').removeClass("col-6");
+    } else if (larguraResize520 >= 521) {
+      $('.icones-rodape').addClass("col-6");
+      $('.rodape-texto').addClass("col-6");
+    };
+  };
+
   $(window).resize(function(){
-    ajustaLarguraMapa();
+    resize1000px();
+    resize520px();
   });
   //Fire it when the page first loads:
-  ajustaLarguraMapa();
+  resize1000px();
+  resize520px();
 
   var posicaoScrollAnterior = window.pageYOffset;
   window.onscroll = function() {
